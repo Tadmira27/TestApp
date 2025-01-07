75251f2fe2bc31bcc142c5fbec77e3ecbc9b4e63
Mithela <mithilamithi94@gmail.com>
2025-01-07 11:16:45 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Select the button element
+const button = document.getElementById('buttonClickMe');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+    alert('Button clicked first time!');
+});
